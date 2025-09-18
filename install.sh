9943990fcd5580516722a8655c147a95af4ef277
Aaganrmu <elmar.wiedemeijer@gmail.com>
2025-09-18 22:10:49 +0200

    chore: easier install

--- install.sh
+++ install.sh
@@ -4,30 +4,8 @@ DEVICE="/media/aaganrmu/CIRCUITPY/"
 
 echo "installing"
 cd source
-echo "transfering boot.py"
-$(cp boot.py $DEVICE)
-echo "transfering code.py"
-$(cp code.py $DEVICE)
-echo "creating folders"
-$(mkdir -p $DEVICE/lib)
-$(mkdir -p $DEVICE/ledz)
-$(mkdir -p $DEVICE/pattern)
-cd lib
 localfiles=$(ls)
 for file in $localfiles; do
-    echo "transfering lib/$file"
-    $(cp $file $DEVICE/lib/)
-done
-cd ../ledz
-$(mkdir -p $DEVICE/ledz)
-localfiles=$(ls)
-for file in $localfiles; do
-    echo "transfering ledz/$file"
-    $(cp $file $DEVICE/ledz/)
-done
-cd ../pattern
-localfiles=$(ls)
-for file in $localfiles; do
-    echo "transfering pattern/$file"
-    $(cp $file $DEVICE/pattern)
+    echo "transfering $file"
+    $(cp -r $file $DEVICE/)
 done
